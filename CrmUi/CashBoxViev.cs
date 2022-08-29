d7e7e911bc6d2cc725f03acf2795db4aaf074b06
AlexFilov <alexfilov.ukraine@gmail.com>
2022-08-29 10:56:12 +0300

    CRM
    CRM

---
+++ CrmUi/CashBoxViev.cs
@@ -0,0 +1,69 @@
+﻿using CrmBl.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CrmUi
+{
+    class CashBoxViev
+    {
+        CashDesk cashDesk;
+        public Label CashDeskName { get; set; }
+        public NumericUpDown Price { get; set; }
+        public ProgressBar QueueLenght {get; set;}
+        public Label LeaveCustomersCount { get; set; }
+
+        public CashBoxViev(CashDesk cashDask, int number, int x, int y)
+        {
+            this.cashDesk = cashDask;
+
+            CashDeskName = new Label();
+            Price = new NumericUpDown();
+            QueueLenght = new ProgressBar();
+            LeaveCustomersCount = new Label();
+
+            CashDeskName.AutoSize = true;
+            CashDeskName.Location = new System.Drawing.Point(x,y);
+            CashDeskName.Name = "label" + number;
+            CashDeskName.Size = new System.Drawing.Size(46, 17);
+            CashDeskName.TabIndex = number;
+            CashDeskName.Text = cashDask.ToString();
+
+            Price.Location = new System.Drawing.Point(x + 70, y);
+            Price.Name = "numericUpDown" + number;
+            Price.Size = new System.Drawing.Size(120, 20);
+            Price.TabIndex = number;
+            Price.Maximum = 1000000000000000;
+
+            QueueLenght.Location = new System.Drawing.Point(x + 250, y);
+            QueueLenght.Maximum = cashDask.MaxQueueLenght;
+            QueueLenght.Name = "progressBar" + number;
+            QueueLenght.Size = new System.Drawing.Size(100, 23);
+            QueueLenght.TabIndex = number;
+            QueueLenght.Value = 0;
+
+            LeaveCustomersCount.AutoSize = true;
+            LeaveCustomersCount.Location = new System.Drawing.Point(x + 400, y);
+            LeaveCustomersCount.Name = "label2" + number;
+            LeaveCustomersCount.Size = new System.Drawing.Size(46, 17);
+            LeaveCustomersCount.TabIndex = number;
+            LeaveCustomersCount.Text =  "";
+
+            cashDask.CheckClosed += CashDask_CheckClosed;
+        }
+
+        private void CashDask_CheckClosed(object sender, Check e)
+        {
+            Price.Invoke((Action)delegate
+            {
+                Price.Value += e.Price;
+                QueueLenght.Value = cashDesk.Count;
+                LeaveCustomersCount.Text = cashDesk.ExitCustomer.ToString();
+            });
+            
+        }
+    }
+}
